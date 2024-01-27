c77d1b27386c95104ab77d9597ab5f101e6c5cff
Mindaugas <mindaugas932@gmail.com>
2024-01-28 00:07:49 +0200

    Update gulpfile to copy images

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,40 @@
 const gulp = require('gulp'),
     nunjucksRender = require('gulp-nunjucks-render'),
     browserSync = require('browser-sync').create(),
+    del = require('del'),
     srcDir = './src',
     destDir = './public';
 
-gulp.task('build', function () {
+gulp.task('clean-resources', function () {
+    return del(destDir + '/images');
+});
+
+gulp.task('copy-resources', function () {
+    return gulp.src(srcDir + '/images/**')
+        .pipe(gulp.dest(destDir + '/images'));
+});
+
+gulp.task('update-resources', gulp.series('clean-resources', 'copy-resources'));
+
+gulp.task('build-html', function () {
     return gulp.src(srcDir + '/*.html')
         .pipe(nunjucksRender({
             path: srcDir,
+            envOptions: {
+                throwOnUndefined: true,
+            },
         }))
         .pipe(gulp.dest(destDir));
 });
 
-gulp.task('watch', function () {
-    gulp.watch(srcDir + '/*.html', gulp.series('build'));
-})
+gulp.task('build', gulp.series('update-resources', 'build-html'));
+
+function watchFiles() {
+    gulp.watch(srcDir + '/images/**', gulp.series('update-resources'));
+    gulp.watch(srcDir + '/**/*.html', gulp.series('build-html'));
+}
+
+gulp.task('watch', watchFiles);
 
 gulp.task('reload', function () {
     browserSync.init({
@@ -23,8 +43,10 @@ gulp.task('reload', function () {
         },
         open: false,
     });
-    gulp.watch(destDir + '/*.html').on('change', browserSync.reload);
-    gulp.watch(srcDir + '/*.html', gulp.series('build'));
-})
 
-gulp.task('default', gulp.series('build'));
+    gulp.watch(destDir).on('change', browserSync.reload);
+
+    watchFiles();
+});
+
+gulp.task('default', gulp.series('build-html'));
